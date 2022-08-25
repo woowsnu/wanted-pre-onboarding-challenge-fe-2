ce35fb237e9defdb38c3f5ea50cb84c3a330bacb
woowsnu <woowsnu@gmail.com>
2022-08-25 23:43:14 +0900

    first commit

---
+++ src/Todo.js
@@ -0,0 +1,44 @@
+// @ts-check
+
+/**
+ * Todo 추가 (단, content는 필수)
+ * @param {string} id 
+ * @param {string} content 
+ * @param {number} category 
+ * @returns 
+ */
+function createTodo(id, content, category) {
+  return;
+}
+
+/**
+ * Todo 조회 (Id로 특정 Todo 조회도 가능)
+ * @param {string} id 
+ * @returns 
+ */
+function readTodo(id) {
+  return;
+}
+
+/**
+ * Todo 수정 (Id는 수정 불가)
+ * @param {string} id 
+ * @param {string} content 
+ * @param {boolean} isCompleted 
+ * @param {number} category 
+ * @param {string} tags 
+ * @returns 
+ */
+function updateTodo(id, content, isCompleted, category, tags) {
+  return;
+}
+
+/**
+ * Todo 삭제 
+ * @param {string} id 
+ * @param {string} tags 
+ * @returns 
+ */
+function deleteTodo(id, tags) {
+  return;
+}
---
+++ src/TypeTodo.ts
@@ -0,0 +1,7 @@
+interface todo {
+  id: string;
+  content: string;
+  isCompleted: boolean;
+  category: number;
+  tags?: string;
+}
